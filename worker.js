aa51401bd5bf4cad106bad8559c37a65c01645a1
Anish Kumar <88476617+AnishKumarKallepalli@users.noreply.github.com>
2024-12-26 18:47:57 -0800

    Shot 7

--- worker.js
+++ worker.js
@@ -1,11 +1,10 @@
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request));
+    event.respondWith(handleRequest(event.request, event));
 });
 
-async function handleRequest(request) {
+async function handleRequest(request, event) {
     try {
-        const NAME_ENTRY = NAME_ENTRY;
-        const EMAIL_ENTRY = EMAIL_ENTRY;
+        const { NAME_ENTRY, EMAIL_ENTRY } = event.env; // Access the secrets
         const formData = await request.formData();
         const name = formData.get('name');
         const email = formData.get('email');
@@ -18,14 +17,8 @@ async function handleRequest(request) {
 
         const response = await fetch(`${googleFormURL}?${params.toString()}`, { method: "POST", mode: "no-cors" });
 
-        // Log the successful submission
-        console.log('Form submitted successfully:', { name, email });
-
         return new Response('Form submitted successfully!', { status: 200 });
     } catch (error) {
-        // Log the error
-        console.error('Error submitting form:', error);
-
         return new Response('Error submitting form', { status: 500 });
     }
 }
